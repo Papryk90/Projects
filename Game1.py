8c315eb1527a9983e079b40117a31c85adb76c2f
Papryk90 <patrykstopczynski@gmail.com>
2022-08-08 13:55:51 +0200

    Add new project

---
+++ Game1.py
@@ -0,0 +1,9 @@
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+movie = input("Enter a movie: ")
+
+print("Roses are " + color)
+print(plural_noun + " are cool")
+print("I love " + movie)
+
